73cf294143bce3973fea81603e6c09c1de8bb60f
mfatihaktas <mfatihaktas@gmail.com>
2023-08-20 13:26:51 -0400

    Refined tests/exp/test_time_to_deanonimize

--- tests/exp/test_time_to_deanonimize.py
+++ tests/exp/test_time_to_deanonimize.py
@@ -12,11 +12,12 @@ def test_plot_avg_time_to_deanonymize_vs_num_servers():
     # idle_time_rv = random_variable.Exponential(mu=1)
     idle_time_rv = random_variable.Uniform(min_value=1, max_value=20)
     num_msgs_to_recv_for_get_request_rv = random_variable.DiscreteUniform(min_value=1, max_value=1)
-    num_target_servers = 1
-    error_percent = 0.05
+    num_target_servers = 2
+    error_percent = 0.2
     num_samples = 5
 
-    log(INFO, "Started",
+    log(
+        INFO, "Started",
         network_delay_rv=network_delay_rv,
         idle_time_rv=idle_time_rv,
         num_msgs_to_recv_for_get_request_rv=num_msgs_to_recv_for_get_request_rv,
@@ -27,6 +28,7 @@ def test_plot_avg_time_to_deanonymize_vs_num_servers():
 
     num_servers_list = []
     E_time_to_deanonymize_list = []
+    std_time_to_deanonymize_list = []
     for num_servers in range(2, 7):
         log(INFO, f">> num_servers= {num_servers}")
         num_clients = num_servers
@@ -43,29 +45,38 @@ def test_plot_avg_time_to_deanonymize_vs_num_servers():
         )
 
         E_time_to_deanonymize = numpy.mean(time_to_deanonymize_list)
-        log(INFO, "",
+        std_time_to_deanonymize = numpy.std(time_to_deanonymize_list)
+        log(
+            INFO, "",
             E_time_to_deanonymize=E_time_to_deanonymize,
+            std_time_to_deanonymize=std_time_to_deanonymize,
         )
 
         num_servers_list.append(num_servers)
         E_time_to_deanonymize_list.append(E_time_to_deanonymize)
+        std_time_to_deanonymize_list.append(std_time_to_deanonymize)
 
-    plot.plot(num_servers_list, E_time_to_deanonymize_list, color=NICE_BLUE, marker="x")
-
-    # TODO: Add stdev with error margin bars.
+    log(
+        INFO, "",
+        num_servers_list=num_servers_list,
+        E_time_to_deanonymize_list=E_time_to_deanonymize_list,
+        std_time_to_deanonymize_list=std_time_to_deanonymize_list,
+    )
+    plot.errorbar(num_servers_list, E_time_to_deanonymize_list, yerr=std_time_to_deanonymize_list, color=NICE_BLUE, marker="x")
 
     fontsize = 14
     plot.xlabel(r"$N_{\mathrm{server}}$", fontsize=fontsize)
     plot.ylabel(r"$E[T_{\mathrm{deanon}}]$", fontsize=fontsize)
-    title = \
-        r"$N_{\mathrm{client}} =$" + fr"${num_clients}$, " + \
-        r"$T_{\mathrm{net}} \sim$" + fr"${network_delay_rv.to_latex()}$, " + \
-        r"$T_{\mathrm{idle}} \sim$" + fr"${idle_time_rv.to_latex()}$, " + \
-        r"$N_{\mathrm{get}} \sim$" + fr"${num_msgs_to_recv_for_get_request_rv.to_latex()}$, " + \
-        r"$N_{\mathrm{target-server}} =$" + fr"${num_target_servers}$" + "\n" \
+    title = (
+        # r"$N_{\mathrm{client}} =$" + fr"${num_clients}$, " + \
+        r"$T_{\mathrm{net}} \sim$" + fr"${network_delay_rv.to_latex()}$, "
+        r"$T_{\mathrm{idle}} \sim$" + fr"${idle_time_rv.to_latex()}$, "
+        r"$N_{\mathrm{get}} \sim$" + fr"${num_msgs_to_recv_for_get_request_rv.to_latex()}$, " + "\n"
+        r"$N_{\mathrm{target-server}} =$" + fr"${num_target_servers}$, "
         r"$N_{\mathrm{samples}} =$" + fr"${num_samples}$"
-    plot.title(title, fontsize=fontsize)  # , y=1.05
-    plot.gcf().set_size_inches(6, 6)
-    plot.savefig("plots/plot_avg_time_to_deanonymize_vs_num_servers.png", bbox_inches="tight")
+    )
+    plot.title(title, fontsize=fontsize)
+    plot.gcf().set_size_inches(6, 4)
+    plot.savefig("plots/plot_time_to_deanon_vs_num_servers.png", bbox_inches="tight")
     plot.gcf().clear()
     log(INFO, "Done.")
