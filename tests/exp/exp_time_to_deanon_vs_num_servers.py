60ba0134bdab5fb72a9881307f88bb718ad16c80
Mehmet Aktas <mfatihaktas@gmail.com>
2023-08-25 21:37:40 -0400

    + num_servers_list = [3, 20, 50, 100, 200, 500, 1000, 1500, 2000, 2500, 3000]

--- tests/exp/exp_time_to_deanon_vs_num_servers.py
+++ tests/exp/exp_time_to_deanon_vs_num_servers.py
@@ -110,8 +110,9 @@ if __name__ == "__main__":
     idle_time_rv_for_target_client = random_variable.Uniform(min_value=4, max_value=6)
     num_msgs_to_recv_for_get_request_rv = random_variable.DiscreteUniform(min_value=1, max_value=1)
     num_target_servers = 2
-    # num_servers_list = list(range(4, 10))
-    num_servers_list = list(range(3, 20))
+    # num_servers_list = list(range(3, 20))
+    # num_servers_list = [3, 10, 20, 50, 100, 200]
+    num_servers_list = [3, 20, 50, 100, 200, 500, 1000, 1500, 2000, 2500, 3000]
     diff_threshold = 0.003
     num_samples = 5
 
