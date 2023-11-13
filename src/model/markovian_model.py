7e70565ca8280c4dcf6bab53e3ffb70145348717
mfatihaktas <mfatihaktas@gmail.com>
2023-11-13 18:53:53 -0500

    Added src/model/markovian_model

---
+++ src/model/markovian_model.py
@@ -0,0 +1,38 @@
+from src.prob import random_variable
+
+
+def prob_n_arrivals(
+    attack_window_length: float,
+    arrival_rate: float,
+    n: int,
+):
+    num_arrivals_rv = random_variable.Poisson(
+        mu=attack_window_length * arrival_rate
+    )
+    return num_arrivals_rv.tail_prob(n)
+
+
+def prob_server_active(
+    network_delay_rv: random_variable.RandomVariable,
+    client_idle_time_rv: random_variable.Exponential,
+    num_msgs_to_recv_for_get_request_rv: random_variable.RandomVariable,
+) -> float:
+    return prob_n_arrivals(
+        attack_window_length=network_delay_rv.max_value,
+        arrival_rate=client_idle_time_rv.mu,
+        # TODO: Attention!
+        n=num_msgs_to_recv_for_get_request_rv.min_value,
+    )
+
+
+def prob_attack_round(
+    network_delay_rv: random_variable.RandomVariable,
+    target_client_idle_time_rv: random_variable.Exponential,
+    num_msgs_to_recv_for_get_request_rv: random_variable.RandomVariable,
+) -> float:
+    return prob_n_arrivals(
+        attack_window_length=network_delay_rv.max_value,
+        arrival_rate=target_client_idle_time_rv.mu,
+        # TODO: Attention!
+        n=num_msgs_to_recv_for_get_request_rv.min_value,
+    )
