d72c1c0013001c1f3cb7872d090dfdbb12456425
mfatihaktas <mfatihaktas@gmail.com>
2023-11-18 11:03:29 -0800

    +  FILE="exp_perf_vs_max_msg_delivery_time"

--- srun.sh
+++ srun.sh
@@ -10,7 +10,8 @@ elif [ $1 = "j" ]; then
   # FILE="exp_perf_vs_num_servers"
   # FILE="exp_perf_vs_detection_gap_exp_factor"
   # FILE="exp_perf_vs_num_servers_excluded_from_threshold"
-  FILE="exp_perf_vs_prob_server_active"
+  # FILE="exp_perf_vs_prob_server_active"
+  FILE="exp_perf_vs_max_msg_delivery_time"
 
   NTASKS=1
   echo "#!/bin/bash
